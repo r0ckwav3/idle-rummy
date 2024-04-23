ff8139d2636fe845b08e820fce782870d9f3c353
r0ckwav3 <peterrockwave@gmail.com>
2024-04-23 11:01:20 -0700

    cards look pretty nice

--- src/MainPanel/Hand.js
+++ src/MainPanel/Hand.js
@@ -1,27 +1,40 @@
 import React, {useState} from 'react';
 
+const card_dims = [200, 300];
+
 export default function CardHand(){
   let [handContents, setHandContents] = useState([])
 
   let cardobs = handContents.map((el, i) => (
-    <Card key = {i+el.suit+el.value} suit={el.suit} value={el.value} />
+    <Card key = {i+el.suit+el.value} suit={el.suit} value={el.value} islast = {(i == handContents.length-1)}/>
   ));
   return (
-    <div className = "cardHand">
+    <div> {/* TEMP */}
       <button onClick = {() => setHandContents([{suit:"heart", value:"9"},{suit:"heart", value:"10"},{suit:"heart", value:"J"},{suit:"heart", value:"Q"},{suit:"heart", value:"K"}])}>
         Make a straght flush
       </button>
       <button onClick = {() => setHandContents([{suit:"spade", value:"4"},{suit:"heart", value:"4"},{suit:"diamond", value:"4"},{suit:"club", value:"4"}])}>
         Make a 4-of-a kind
       </button>
-      {cardobs}
+      <button onClick = {() => setHandContents([1,2,3,4,5,6,7,8,9,10,"J","Q","K"].map(v => {return {suit:"spade", value:v}}))}>
+        Make a suit
+      </button>
+      <div className = "cardHand">
+        {cardobs}
+      </div>
     </div>
   );
 }
 
-function Card({suit, value}){
+function Card({suit, value, islast}){
+  let mystyles = {"width": 200};
+  if (islast){
+    mystyles["min-width"] = 200;
+  }else{
+    mystyles["min-width"] = 0;
+  }
   return (
-  <div className = "card">
+    <div className = "card" style={mystyles}>
     {suit_to_symbol(suit)}
     <br/>
     {value}
