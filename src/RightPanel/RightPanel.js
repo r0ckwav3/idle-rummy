f090cc1b67b195755e48b253c3c7697f4f190c6b
r0ckwav3 <peterrockwave@gmail.com>
2024-05-28 17:40:22 -0700

    move upgrades into flexboxes

--- src/RightPanel/RightPanel.js
+++ src/RightPanel/RightPanel.js
@@ -2,37 +2,36 @@ import MilestoneBox from "../Utils/MilestoneBox.js"
 
 import "./styles.css";
 
+const upgrades = [
+  ["hand_size_1", "deck_cooldown_1", "ofakind_double_1", "straight_double_1"],
+  [null, "deck_cooldown_2", null, null],
+  ["hand_size_2", "deck_cooldown_3", "ofakind_double_2", "straight_double_2"],
+  ["golden_unlock", "deck_cooldown_4", null, null],
+  [null, "deck_cooldown_5", "ofakind_double_3", "straight_double_3"],
+  ["hand_size_3", "ofakind_double_4", "straight_double_4"],
+  ["sort_hand", null, null],
+  ["hand_size_4", "ofakind_double_5", "straight_double_5"],
+  ["hand_size_5"],
+]
+
 export default function RightPanel(){
+  let rows = upgrades.map((row, i) => {
+    let milestones = row.map(name => {
+      if (name === null){
+        return (<div className="milestoneBoxFiller"/>);
+      }else{
+        return (<MilestoneBox milestoneName = {name} key={name}/>);
+      }
+    });
+    return (
+      <div className="upgradeRow" key={i}>
+        {milestones}
+      </ div>
+    );
+  });
   return (
     <div className="rightPanel">
-      I'm in the right panel
-      <br />
-      <MilestoneBox milestoneName = "deck_cooldown_1"/>
-      <MilestoneBox milestoneName = "deck_cooldown_2"/>
-      <MilestoneBox milestoneName = "deck_cooldown_3"/>
-      <MilestoneBox milestoneName = "deck_cooldown_4"/>
-      <MilestoneBox milestoneName = "deck_cooldown_5"/>
-      <br />
-      <MilestoneBox milestoneName = "hand_size_1"/>
-      <MilestoneBox milestoneName = "hand_size_2"/>
-      <MilestoneBox milestoneName = "hand_size_3"/>
-      <MilestoneBox milestoneName = "hand_size_4"/>
-      <MilestoneBox milestoneName = "hand_size_5"/>
-      <br />
-      <MilestoneBox milestoneName = "ofakind_double_1"/>
-      <MilestoneBox milestoneName = "ofakind_double_2"/>
-      <MilestoneBox milestoneName = "ofakind_double_3"/>
-      <MilestoneBox milestoneName = "ofakind_double_4"/>
-      <MilestoneBox milestoneName = "ofakind_double_5"/>
-      <br />
-      <MilestoneBox milestoneName = "straight_double_1"/>
-      <MilestoneBox milestoneName = "straight_double_2"/>
-      <MilestoneBox milestoneName = "straight_double_3"/>
-      <MilestoneBox milestoneName = "straight_double_4"/>
-      <MilestoneBox milestoneName = "straight_double_5"/>
-      <br />
-      <MilestoneBox milestoneName = "golden_unlock"/>
-      <MilestoneBox milestoneName = "sort_hand"/>
+      {rows}
     </div>
   )
 }
