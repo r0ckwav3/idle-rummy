cff7c4a5dba5112fe1da6e3c8829a4ce74fbc899
r0ckwav3 <peterrockwave@gmail.com>
2024-06-14 21:41:09 -0700

    show cash out button when unlocked

--- src/MainPanel/CollectButtons.js
+++ src/MainPanel/CollectButtons.js
@@ -1,29 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import eventManager from '../Utils/EventManager.js';
+import useEventHook from '../Utils/EventHooks.js';
+import {useMilestone, useMilestoneActive} from '../Utils/MilestoneHooks.js'
 
 export default function CollectButtons(){
+  const ascensionUpgradeActive = useMilestoneActive({milestoneName: 'unlock_ascension'});
+  const showCashOut = ascensionUpgradeActive;
+
   return (
     <div className="collectButtons">
       <SubmitHandButton/>
+      {showCashOut ? <CashOutButton/> : ""}
     </div >
   )
+}
+
+function CashOutButton(){
+  // TODO: set up default value
+  let [buttonText, setButtonText] = useState("Cash Out (+0)");
+
+  useEventHook("updateAscendValue", e => {
+    let value = e.value;
+    setButtonText(`Cash Out (+${value})`);
+  });
+
+  function buttonClicked() {
+    eventManager.sendEvent({name: "attemptSubmitHand"});
+  }
+
+  return (
+      <button className = "cashOutButton" onClick = {buttonClicked}>
+        <code>
+          {buttonText}
+        </code>
+      </button>
+    )
 }
 
 function SubmitHandButton(){
   let [buttonText, setButtonText] = useState("+0 Chips");
   let [isActive, setIsActive] = useState();
 
-  useEffect(()=>{
-    const eventHook = eventManager.createHook("updateHandValue", e => {
-      let value = e.value;
-      setButtonText(`+${value} Chips`);
-      setIsActive(value !== 0);
-    });
-
-    return () => {
-      eventManager.removeHook(eventHook);
-    };
+  useEventHook("updateHandValue", e => {
+    let value = e.value;
+    setButtonText(`+${value} Chips`);
+    setIsActive(value !== 0);
   });
 
   function buttonClicked() {
